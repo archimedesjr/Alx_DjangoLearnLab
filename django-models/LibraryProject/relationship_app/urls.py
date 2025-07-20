f67b5463b5b60cf46da28e9cce3aed9a9b949c73
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-07-20 23:04:53 +0100

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import LibraryDetailView
 from .views import list_books
+from relationship_app import views
 
 urlpatterns = [
     path('library/', LibraryDetailView.as_view(), name='library-detail'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
@@ -21,3 +24,18 @@ class LibraryDetailView(DetailView):
     context = super().get_context_data(**kwargs)  # Get default context data
     context['books'] = self.object.books.all()
     return context
+  
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+    
+# def register(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('login')
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'relationship_app/register.html', {'form': form})
