127d910c23cc899dd4232ab59e07c6a4c56ee9dc
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-07-20 22:00:49 +0100

    Added Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,7 +12,6 @@ def book_list(request):
 class LibraryDetailView(DetailView):
   """A class-based view for displaying details of a specific Library."""
   model = Library
-  template_name = 'relationship_app/list_books.html'
   template_name = 'relationship_app/library_detail.html'
   context_object_name = "library"
 
