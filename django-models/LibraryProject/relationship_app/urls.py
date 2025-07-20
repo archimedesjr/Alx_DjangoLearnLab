83d5bf735464e026213840337cdab8dbebc48dd4
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-07-20 23:49:24 +0100

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import LibraryDetailView
+from . import views
 from .views import list_books
-from relationship_app import views
+# from relationship_app import views
 
 urlpatterns = [
-    path('library/', LibraryDetailView.as_view(), name='library-detail'),
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout'))
+    path('logout/', LogoutView.as_view(template_name='logout')),
     path('register/', views.register, template_name='register'),
-
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
