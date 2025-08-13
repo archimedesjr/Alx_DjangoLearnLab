de68ae83d8997f1cf2a986189816764cf4f76223
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-08-13 10:45:34 +0100

    Fixed errors in settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -13,12 +13,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+BASE_DIR = Path(__file__).resolve().parent.parent
 from dotenv import load_dotenv
 
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
