b94a66f5302f5cdacca3190f2f187ae605ed5ae7
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-08-14 02:20:46 +0100

    Fixed errors in views.py, urls.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -54,16 +54,15 @@ class PostListView(ListView):
     ordering = ['-published_date']
 
     def get_queryset(self):
-        queryset = super().get_queryset()
         query = self.request.GET.get('q')
-        
+
         if query:
-            queryset = queryset.filter(
+            return Post.objects.filter(
                 Q(title__icontains=query) |
                 Q(content__icontains=query) |
                 Q(tags__name__icontains=query)
             ).distinct()
-        return queryset
+        return Post.objects.all()
 
 # Retrieve a single blog by ID
 class PostDetailView(DetailView):
