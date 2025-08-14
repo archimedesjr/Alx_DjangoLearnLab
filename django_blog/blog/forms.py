0ded7d0bf164bb3029577c2177a5dda4894d2d69
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-08-14 02:02:23 +0100

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -24,6 +24,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple()
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
