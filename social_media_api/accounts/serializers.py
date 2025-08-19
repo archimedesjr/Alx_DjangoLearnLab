4cc03f027cbf2f7133646dfe88172e140b1b424e
Temitayo Shittu <temitayo.shittu7@gmail.com>
2025-08-19 22:32:28 +0100

    Project Setup and User Authentication for a Social Media API

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)  # ✅ explicit CharField
+    password = serializers.CharField() # ✅ explicit CharField
 
     class Meta:
         model = get_user_model()
